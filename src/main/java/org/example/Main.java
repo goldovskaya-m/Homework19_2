e4d4c8485be7f556bdc83335be355b5ab1c1fcc3
goldovskaya-m <ariya-aaa@yandex.ru>
2024-08-15 09:14:57 +0300

    Commit Done +

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,19 +8,20 @@ import java.util.Collections;
 import java.util.List;
 
 public class Main {
+
     private static final List<Integer> nums =
             new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 5, 7));
-    private static final List<String> words = new ArrayList<>(List.of("abc", "abc", "bcb", "bcb", "e"));
+    private static final List<String> words = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
 
     public static void main(String[] args) {
-        task1();
+        task3();
         System.out.println("Homework19_2");
     }
 
     public static void task1() {
         for (int num : nums) {
             if (num % 2 != 0) {
-                System.out.println(num);
+                System.out.println("напечатать только нечетные числа " + num);
             }
         }
     }
@@ -29,17 +30,29 @@ public class Main {
         Collections.sort(nums);
         int prevNum = Integer.MIN_VALUE;
         for (int num : nums) {
-            //if (num % 2 = 0 && num != prevNum) {
-            System.out.println(num);
-            prevNum = num;
+            if (num % 2 == 0 && num != prevNum) {
+                System.out.println("напечатать только четные числа без повторений в порядке возрастания - " + num);
+                prevNum = num;
+            }
         }
     }
+
     public static void task3() {
         Set<String> uniqueWords = new HashSet<>(words);
-        System.out.println(uniqueWords);
+        System.out.println("все уникальные слова из списка слов " + uniqueWords);
     }
+
     public static void task4() {
-        Set<String>uniqueWords = new HashSet<>(words);
-        System.out.println(words.size()- uniqueWords.size());
+        Set<String> uniqueWords = new HashSet<>(words);
+        System.out.println(" Количество повторов " + (words.size() - uniqueWords.size()));
+
     }
 }
+
+
+
+
+
+
+
+
